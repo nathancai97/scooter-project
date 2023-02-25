0d8c441101d32d9109c452bf76b832e73e67bfbb
Nathan Cai <nathan97cai@gmail.com>
2023-02-25 18:19:07 -0500

    completed user and tests

--- tests/User.test.js
+++ tests/User.test.js
@@ -1,13 +1,34 @@
-const User = require('../src/User')
+const User = require("../src/User");
 
-// User tests here
+describe("User", () => {
+  let user;
 
-// test username
+  beforeEach(() => {
+    user = new User("Nathan", "password123", 25);
+  });
 
-// test password
+  test("should create a new user with the correct properties", () => {
+    expect(user.username).toBe("Nathan");
+    expect(user.password).toBe("password123");
+    expect(user.age).toBe(25);
+    expect(user.loggedIn).toBe(false);
+  });
 
-// test age
+  test("should be able to log in with the correct password", () => {
+    user.login("password123");
+    expect(user.loggedIn).toBe(true);
+  });
 
-// test login
+  test("should throw an error when logging in with an incorrect password", () => {
+    expect(() => {
+      user.login("password12");
+    }).toThrow("Username or password is incorrect");
+    expect(user.loggedIn).toBe(false);
+  });
 
-// test logout
+  test("should be able to log out", () => {
+    user.login("password123");
+    user.logout();
+    expect(user.loggedIn).toBe(false);
+  });
+});
