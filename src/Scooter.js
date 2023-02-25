038aaec06e5764de4f187f27a1d437d2c39108d1
Nathan Cai <nathan97cai@gmail.com>
2023-02-25 00:09:52 -0500

    completed scooter.js and testing

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,54 @@
-class Scooter{
-  // scooter code here
+class Scooter {
+  constructor(station) {
+    this.station = station;
+    this.user = null;
+    this.serial = 1;
+    this.nextSerial = 1;
+    this.charge = 100;
+    this.isBroken = false;
+  }
+
+  rent(user) {
+    if (this.charge >= 20 && !this.isBroken) {
+      this.station = null;
+      this.user = user;
+    } else if (this.charge < 20) {
+      throw new Error(`Scooter needs to charge.`);
+    } else {
+      throw new Error(`Scooter needs repair.`);
+    }
+  }
+
+  dock(station) {
+    this.station = station;
+    this.user = null;
+  }
+
+  recharge() {
+    if (this.charge < 100) {
+      console.log(`Scooter is charging...`);
+      setInterval(() => {
+        if (this.charge < 100) {
+          this.charge += 10;
+          console.log(`Scooter battery percentage at ${this.charge}...`);
+        } else {
+          console.log(`Scooter has finished charging!`);
+        }
+      }, 2000);
+    }
+  }
+
+  requestRepair() {
+    if (!this.isBroken) {
+      return `Scooter is working!`;
+    }
+
+    console.log(`Scooter repair has been requested.`);
+    setTimeout(() => {
+      console.log(`Scooter ${this.serial} repair completed!`);
+    }, 5000);
+    this.isBroken = false;
+  }
 }
 
-
-module.exports = Scooter
+module.exports = Scooter;
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,25 +1,73 @@
-const Scooter = require('../src/Scooter')
-const User = require('../src/User')
+const Scooter = require("../src/Scooter");
 
-//typeof scooter === object
-describe('scooter object', () => {
-  test('does something', () => {
-    // edit this to be a real test!
-    expect(false).toEqual(true);
-  }
-)
-})
+describe("Scooter", () => {
+  let scooter;
+  beforeEach(() => {
+    scooter = new Scooter("Station A");
+  });
 
-//Method tests
-describe('scooter methods', () => {
-  // tests here!
+  test("constructor initializes properties correctly", () => {
+    expect(scooter.station).toBe("Station A");
+    expect(scooter.user).toBeNull();
+    expect(scooter.charge).toBe(100);
+    expect(scooter.isBroken).toBe(false);
+  });
 
-  //rent method
+  // rent method
+  test("renting a scooter updates its properties correctly", () => {
+    scooter.name = "Nathan";
+    scooter.rent("Nathan");
+    expect(scooter.station).toBeNull();
+    expect(scooter.user).toBe("Nathan");
+  });
 
-  //dock method
+  test("renting a scooter with low charge throws an error", () => {
+    scooter.name = "Nathan";
+    scooter.charge = 10;
+    expect(() => {
+      scooter.rent("Nathan");
+    }).toThrowError("Scooter needs to charge.");
+  });
 
-  //requestRepair method
+  test("renting a broken scooter throws an error", () => {
+    scooter.name = "Nathan";
+    scooter.isBroken = true;
+    expect(() => {
+      scooter.rent("Nathan");
+    }).toThrowError("Scooter needs repair.");
+  });
 
-  //charge method
+  // dock method
+  test("docking a scooter updates its properties correctly", () => {
+    scooter.name = "Nathan";
+    scooter.rent("Nathan");
+    scooter.dock("Station B");
+    expect(scooter.station).toBe("Station B");
+    expect(scooter.user).toBeNull();
+  });
 
-})
+  // requestRepair method
+  test("repairing a scooter updates isBroken", () => {
+    scooter.isBroken = true;
+    jest.useFakeTimers();
+    scooter.requestRepair();
+    jest.runAllTimers();
+    expect(scooter.isBroken).toBe(false);
+    jest.useRealTimers();
+  });
+
+  test("should let user know that scooter isn't broken", () => {
+    scooter.isBroken = false;
+    expect(scooter.requestRepair()).toBe("Scooter is working!")
+  })
+
+  // charge method
+  test("charging scooter should charge correctly", () => {
+    scooter.charge = 50;
+    jest.useFakeTimers();
+    scooter.recharge();
+    jest.advanceTimersByTime(10000);
+    expect(scooter.charge).toBe(100);
+    jest.useRealTimers();
+  });
+});
